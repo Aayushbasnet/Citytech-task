6b40eb7cd6f6289a80189194b1227950952b80ab
Aayush Basnet <aayushbasnet07@gmail.com>
2022-09-27 23:10:37 +0545

    Clean code in task 5

--- Task 5/index.js
+++ Task 5/index.js
@@ -1,5 +1,37 @@
 let submitBtn = document.querySelector('#submitBtn');
 let employeeForm = document.querySelector('#employeeForm');
+
+// display data on table
+let allData = JSON.parse(localStorage.getItem("All Data"));
+// console.log("aagadi ko data: ", allData);
+if(allData == null){
+    allData = [];
+};
+
+// display data in table
+var tableData = allData;
+let tableBody = document.querySelector('#tableBody');
+for(let i=0; i < tableData.length; i++){
+    // console.log("column length", Object.keys(tableData[i]).length);
+    let row = document.createElement("tr");
+    for(let j=0; j < Object.keys(tableData[i]).length; j++){
+        row.innerHTML = `
+            <td>${i+1}</td>
+            <td>${tableData[i].fname}</td>
+            <td>${tableData[i].lname}</td>
+            <td>${tableData[i].email}</td>
+            <td>${tableData[i].gender}</td>
+            <td>${tableData[i].phoneNumber}</td>
+            <td>${tableData[i].position}</td>
+            <td>${tableData[i].enrollmentDate}</td>
+            <td>${tableData[i].interest}</td>
+            <td>${tableData[i].remark}</td>
+        `;
+    };
+    tableBody.appendChild(row);
+};
+
+// form submission
 employeeForm.addEventListener('submit', (e)=>{
     // e.preventDefault();
     let fname = employeeForm.elements['first-name'];
@@ -19,7 +51,6 @@ employeeForm.addEventListener('submit', (e)=>{
             interests.push(interest[i].value);
         };
     };
-    // console.log("Interest:",interests);
 
     // creating object literals from user data 
     var UserData = {
@@ -35,16 +66,11 @@ employeeForm.addEventListener('submit', (e)=>{
         "remark":remark.value
     };
 
-    // localstorage data array
     // retrive old data from localstorage
     let allData = JSON.parse(localStorage.getItem("All Data"));
-    // console.log("aagadi ko data: ", allData);
     if(allData == null){
         allData = [];
     };
-
-    // console.log("Old data : ",allData);
-    // profileData.push(UserData);
 
     // storing data in localstorage
     localStorage.setItem("New Data", JSON.stringify(UserData)); // new user data
@@ -52,58 +78,6 @@ employeeForm.addEventListener('submit', (e)=>{
     // console.log("All data :", allData);
     localStorage.setItem("All Data", JSON.stringify(allData));
 
-    // // display data in table
-    // var tableData = allData;
-    // // console.log(tableData, " length: ", tableData.length);
-    // let tableBody = document.querySelector('#tableBody');
-    // for(let i=0; i < tableData.length; i++){
-    //     // console.log("column length", Object.keys(tableData[i]).length);
-    //     let row = document.createElement("tr");
-    //     for(let j=0; j < Object.keys(tableData[i]).length; j++){
-    //         row.innerHTML = `
-    //             <td>${i+1}</td>
-    //             <td>${tableData[i].fname}</td>
-    //             <td>${tableData[i].lname}</td>
-    //             <td>${tableData[i].email}</td>
-    //             <td>${tableData[i].gender}</td>
-    //             <td>${tableData[i].phoneNumber}</td>
-    //             <td>${tableData[i].position}</td>
-    //             <td>${tableData[i].enrollmentDate}</td>
-    //             <td>${tableData[i].interest}</td>
-    //             <td>${tableData[i].remark}</td>
-    //         `;
-    //     };
-    //     tableBody.appendChild(row);
-    // };
+    // alert success result
     alert("Successfully submitted");
-    // let fname = document.querySelector()
 });
-// display data on table
-let allData = JSON.parse(localStorage.getItem("All Data"));
-    // console.log("aagadi ko data: ", allData);
-    if(allData == null){
-        allData = [];
-    };
-
-    // display data in table
-    var tableData = allData;
-    let tableBody = document.querySelector('#tableBody');
-    for(let i=0; i < tableData.length; i++){
-        // console.log("column length", Object.keys(tableData[i]).length);
-        let row = document.createElement("tr");
-        for(let j=0; j < Object.keys(tableData[i]).length; j++){
-            row.innerHTML = `
-                <td>${i+1}</td>
-                <td>${tableData[i].fname}</td>
-                <td>${tableData[i].lname}</td>
-                <td>${tableData[i].email}</td>
-                <td>${tableData[i].gender}</td>
-                <td>${tableData[i].phoneNumber}</td>
-                <td>${tableData[i].position}</td>
-                <td>${tableData[i].enrollmentDate}</td>
-                <td>${tableData[i].interest}</td>
-                <td>${tableData[i].remark}</td>
-            `;
-        };
-        tableBody.appendChild(row);
-    };
